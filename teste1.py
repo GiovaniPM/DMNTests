204b503e7468a91e569ec66e4b9240d14543b595
Giovani <giovanipm@gmail.com>
2021-03-24 14:52:32 -0300

    giova(DESKTOP-PG18CKU) 210324145232

---
+++ teste1.py
@@ -0,0 +1,14 @@
+import pyautogui, sys
+
+from pymsgbox import *
+
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    alert(text='', title='', button='OK')
+    print('\n')
